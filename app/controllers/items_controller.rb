1f5978dd5423adbc37a01972f11a1b536b8cac41
shuuuyan <shuchan1023@gmail.com>
2021-03-11 16:02:32 +0900

    編集機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,7 @@
 class ItemsController < ApplicationController
 
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_prototype, except: [:index, :new, :create]
 
   def index
     @items = Item.order("created_at DESC")         #新規投稿順
@@ -20,19 +21,27 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
   end
 
   def update
+    if @item.update(item_params)
+      redirect_to item_path(@item)
+   else 
+     render :edit
+   end
   end
-
 
   private
   
   def item_params
     params.require(:item).permit(:image, :name, :description, :category_id, :shippingarea_id, :condition_id, :shippingday_id, :price, :delivery_fee_id).merge(user_id: current_user.id)
   end
+
+  def set_prototype
+    @item = Item.find(params[:id])
+  end
+  
 end
